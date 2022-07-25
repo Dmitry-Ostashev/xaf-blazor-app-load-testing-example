392671e4b8f48fe0e333f53cfbdce83dd5d4b4b4
Dmitry-Ostashev <dmitry.ostashev@devexpress.com>
2022-07-25 18:31:40 +0400

    run with docker-compose

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,5 @@
 #!/bin/sh
 
-export CONNECTION_STRING=MySqlConnectionString
-
 dotnet LoadTestingApp.Blazor.Server.dll --updateDatabase --forceUpdate --silent
 
 dotnet LoadTestingApp.Blazor.Server.dll
