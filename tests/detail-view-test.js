5079ca44a0b0f3f1eb61ffca1de647cd068207f9
Dmitry-Ostashev <dmitry.ostashev@devexpress.com>
2022-08-17 17:23:59 +0400

    rework test

--- tests/detail-view-test.js
+++ tests/detail-view-test.js
@@ -1,97 +1,40 @@
 async function detailViewTest(page) {
-    const actionWithConnectionCheck = async (action) => {
-        let attempt = 3;
-
-        while (attempt > 0) {
+    const testFunc = async () => {
+        for (let i = 0; i < 20; i++) {
             try {
-                await page.waitForSelector('#blazor-reconnect-ui[style*="display: none;"]', { timeout: 45000 });
+                await page.waitForSelector('.main-content>.grid-content .dxbs-grid .card', { timeout: 30000 });
+                await page.waitForSelector('.xaf-loading-content[style*="hidden"]', { timeout: 30000 });
 
-                attempt = 0;
-            }
-            catch (e) {
-                console.log(`Reconnection attempt ${attempt}`);
+                await page.waitForTimeout(2000);
 
-                await page.waitForTimeout(10000);
-                attempt--;
+                await page.waitForXPath(`//td[contains(., 'Employee ${i}#')]`, { timeout: 30000 });
 
-                if (attempt < 1)
-                    throw e;
-            }
-        }
+                await page.waitForTimeout(1000);
 
-        await action();
-    }
-
-    const tryClickRow = async (rowNumber, attempts) => {
-        let attempt = attempts;
-
-        while (attempt > 0) {
-            try {
-                const row = await page.$x(`//td[contains(., 'Employee ${rowNumber}#')]`, { timeout: 30000 });
+                const row = await page.$x(`//td[contains(., 'Employee ${i}#')]`);
 
                 await row[0].click();
 
-                return;
+                await page.waitForSelector('input[name="FirstName"]', { timeout: 30000 });
+                await page.waitForSelector('.xaf-loading-content[style*="hidden"]', { timeout: 30000 });
+
+                await page.waitForTimeout(3000);
+                await page.evaluate('Blazor.navigateTo("Employee_ListView")')
             }
             catch (e) {
-                console.log(`Click attempt ${attempt}`);
+                try {
+                    await page.waitForSelector('#blazor-reconnect-ui[style*="display: block;"]', { timeout: 10000 });
 
-                await page.waitForTimeout(8000);
-                attempt--;
+                    console.log('Reconnect ... ');
 
-                if (attempt < 1)
+                    await page.waitForSelector('#blazor-reconnect-ui[style*="display: none;"]', { timeout: 45000 });
+                    await page.waitForSelector('.main-window-template-content', { timeout: 30000 });
+                    await page.evaluate('Blazor.navigateTo("Employee_ListView")');
+                    await page.waitForSelector('.main-content>.grid-content .dxbs-grid .card', { timeout: 30000 });
+                }
+                catch (err) {
                     throw e;
-            }
-        }
-    }
-    const testFunc = async () => {
-        for (let i = 0; i < 20; i++) {
-            await actionWithConnectionCheck(async () => await page.waitForSelector('.xaf-loading-content[style*="hidden"]', { timeout: 30000 }));
-            
-            await page.waitForTimeout(2000);
-            // await page.waitForSelector('.xaf-loading-content[style*="hidden"]', { timeout: 30000 });
-            try {
-                await actionWithConnectionCheck(async () => await page.waitForSelector('.main-content>.grid-content .dxbs-grid .card', { timeout: 30000 }));
-            }
-            catch (e) {
-                const url = await page.url();
-
-                if (!url.endsWith('Employee_ListView'))
-                    await page.goBack();
-                else
-                    throw e;
-            }
-            
-            await actionWithConnectionCheck(async () => await page.waitForXPath(`//td[contains(., 'Employee ${i}#')]`, { timeout: 30000 }));
-
-            // const row = await page.$x(`//td[contains(., 'Employee ${i}#')]`, { timeout: 30000 });
-
-            await actionWithConnectionCheck(async () => await tryClickRow(i, 3));
-
-            await page.waitForTimeout(5000);
-
-            const loadingContent = await page.$('.xaf-loading-content');
-
-            if (loadingContent)
-                await page.waitForSelector('.xaf-loading-content[style*="hidden"]', { timeout: 40000 });
-
-            await page.waitForSelector('input[name="FirstName"]', { timeout: 30000 });
-            
-            await page.goBack();
-            await page.waitForTimeout(2000);
-
-            const url = await page.url();
-
-            try {
-                await page.waitForFunction('/Employee_ListView/.test(window.location.pathname)', { timeout: 30000 });
-            }
-            catch (e) {
-                console.log(`Navigation issue: ${url}`);
-
-                if (!url.endsWith('Employee_ListView'))
-                    await page.goBack();
-                else
-                    throw e;
+                }
             }
         }
     }
