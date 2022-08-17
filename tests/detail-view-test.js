a4fbec986283a8d7fe7f29c1662917cebb0a9f18
Dmitry-Ostashev <dmitry.ostashev@devexpress.com>
2022-08-17 18:35:25 +0400

    try another navigation

--- tests/detail-view-test.js
+++ tests/detail-view-test.js
@@ -2,35 +2,40 @@ async function detailViewTest(page) {
     const testFunc = async () => {
         for (let i = 0; i < 20; i++) {
             try {
+                await page.waitForSelector('#blazor-reconnect-ui[style*="display: none;"]', { timeout: 45000 });
+
                 await page.waitForSelector('.main-content>.grid-content .dxbs-grid .card', { timeout: 30000 });
                 await page.waitForSelector('.xaf-loading-content[style*="hidden"]', { timeout: 30000 });
 
-                await page.waitForTimeout(2000);
+                await page.waitForTimeout(3000);
 
-                await page.waitForXPath(`//td[contains(., 'Employee ${i}#')]`, { timeout: 30000 });
+                await page.evaluate(`Blazor.navigateTo("Employee_DetailView/${ + 1}")`);
+                // const row = await page.waitForXPath(`//td[contains(., 'Employee ${i}#')]`, { timeout: 30000 });
 
-                await page.waitForTimeout(1000);
+                // await row[0].click();
 
-                const row = await page.$x(`//td[contains(., 'Employee ${i}#')]`);
-
-                await row[0].click();
-
-                await page.waitForSelector('input[name="FirstName"]', { timeout: 30000 });
+                await page.waitForSelector('#blazor-reconnect-ui[style*="display: none;"]', { timeout: 45000 });
+                await page.waitForSelector('input[name="Person.FirstName"]', { timeout: 30000 });
                 await page.waitForSelector('.xaf-loading-content[style*="hidden"]', { timeout: 30000 });
 
                 await page.waitForTimeout(3000);
                 await page.evaluate('Blazor.navigateTo("Employee_ListView")')
             }
             catch (e) {
+                await page.waitForTimeout(1000);
+
                 try {
                     await page.waitForSelector('#blazor-reconnect-ui[style*="display: block;"]', { timeout: 10000 });
 
                     console.log('Reconnect ... ');
 
                     await page.waitForSelector('#blazor-reconnect-ui[style*="display: none;"]', { timeout: 45000 });
+
                     await page.waitForSelector('.main-window-template-content', { timeout: 30000 });
                     await page.evaluate('Blazor.navigateTo("Employee_ListView")');
                     await page.waitForSelector('.main-content>.grid-content .dxbs-grid .card', { timeout: 30000 });
+                    
+                    console.log('Reconnection success ... ');
                 }
                 catch (err) {
                     throw e;
